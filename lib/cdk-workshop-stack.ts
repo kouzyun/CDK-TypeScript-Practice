eb39d7dec5c5339a0741a328af7bf30bc5df5ccc
kouzyun <harakoji.0514@gmail.com>
2024-07-22 16:37:52 +0900

    first commit

---
+++ lib/cdk-workshop-stack.ts
@@ -0,0 +1,42 @@
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+
+import { readFileSync } from "fs";
+
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "BlogVpc", {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      vpcName: 'BlogVpc'
+    });
+
+    // EC2 インスタンスの宣言を準備
+    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
+      // EC2 インスタンスを起動する VPC を設定
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      // AmazonLinuxImage インスタンスを生成し、AMI を設定
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      // EC2 インスタンスを配置するサブネットを指定
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+
+    const script = readFileSync("./lib/resources/user-data.sh", "utf8");
+    // EC2 インスタンスにユーザーデータを追加
+    webServer1.addUserData(script);
+
+
+    // port80, 全ての IP アドレスからのアクセスを許可
+    webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+    
+    // EC2 インスタンスアクセス用の IP アドレスを出力
+    new CfnOutput(this, "WordpressServer1PublicIPAddress", {
+      value: `http://${webServer1.instancePublicIp}`,
+    });
+  }
+}
